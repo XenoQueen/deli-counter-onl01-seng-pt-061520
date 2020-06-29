fb87d0e494bdd7e1ff258b255ba431a92c656f7d
Maika Kailani <queenmab051@gmail.com>
2020-06-29 18:55:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,31 @@
+katz_deli = []
 
-def line(array)
-  if array.length == 0
+def line(katz_deli)
+  if katz_deli.count == 0
     puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      line << " #{index+1}. #{name}"
+    end
+    puts line
   end
-else
-  puts "The line is currently:"
-  array.each_with_index do |x, y|
-    
 end
 
+def take_a_number(katz_deli, person)
+  if katz_deli.count == 0
+    katz_deli = katz_deli.push(person)
+    puts "Welcome, #{katz_deli[0]}. You are number #{katz_deli.count} in line."
+  else
+    new_line = katz_deli.push(person)
+    puts "Welcome, #{person}. You are number #{katz_deli.count} in line."
+  end
+end
 
+def now_serving(katz_deli)
+  if katz_deli.count == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
