8a74f83af6065f15f0f3690c062fd4c5c7a19328
Maika Kailani <queenmab051@gmail.com>
2020-06-29 18:56:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,13 +12,13 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, person)
+def take_a_number(katz_deli, customer)
   if katz_deli.count == 0
-    katz_deli = katz_deli.push(person)
+    katz_deli = katz_deli.push(customer)
     puts "Welcome, #{katz_deli[0]}. You are number #{katz_deli.count} in line."
   else
-    new_line = katz_deli.push(person)
-    puts "Welcome, #{person}. You are number #{katz_deli.count} in line."
+    new_line = katz_deli.push(customer)
+    puts "Welcome, #{customer}. You are number #{katz_deli.count} in line."
   end
 end
 
